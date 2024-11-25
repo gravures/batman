6cf8335d5609b102fbd10dc8f55e79d7492bb590
Gilles Coissac <gilles@ideographe.fr>
2024-11-25 12:16:09 +0100

    chore: fix copyright header

--- system.py
+++ system.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2024 - Gilles Coissac
 # This file is part of Batman program.
 #
-# Lyndows is free software: you can redistribute it and/or modify
+# Batman is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License,
 # or (at your option) any later version.
 #
-# Lyndows is distributed in the hope that it will be useful,
+# Batman is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Lyndows. If not, see <https://www.gnu.org/licenses/>
+# along with Batman. If not, see <https://www.gnu.org/licenses/>
 from __future__ import annotations
 
 import getpass
